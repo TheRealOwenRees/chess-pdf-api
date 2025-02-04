4dbf0b2def1078f964e4034e7268ac9742713c96
Owen <owenrees82@gmail.com>
2025-02-04 14:44:03 +0100

    logging updated for discord

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,53 @@
+import * as process from 'node:process'
+
+type DiscordLogType = 'error' | 'info'
+
+interface IDiscordLog {
+  type: DiscordLogType
+  message: string
+}
+
+const embedColours: Record<DiscordLogType, number> = {
+  error: 0xff0000,
+  info: 0x00ff00
+}
+
 const logger = {
   info: (...params: string[]) => {
-    console.log(...params)
+    if (process.env.NODE_ENV !== 'production') console.log(...params)
   },
   error: (...params: string[] | Error[]) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'production') console.error(...params)
+  },
+  discord: async ({ type, message }: IDiscordLog) => {
+    if (process.env.NODE_ENV === 'production') {
+      const date = new Date()
+      const timestamp = date.toISOString()
+      const formattedTimestamp = date.toString()
+      const messageTypeTitle = type.charAt(0).toUpperCase() + type.slice(1)
+
+      await fetch(process.env.DISCORD_ERROR_LOG_WEBHOOK as string, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          embeds: [
+            {
+              title: messageTypeTitle,
+              color: embedColours[type],
+              fields: [
+                { name: 'Timestamp', value: `${timestamp}\n${formattedTimestamp}` },
+                { name: messageTypeTitle, value: message }
+              ]
+            }
+          ]
+        })
+      })
+      return true
+    } else {
+      return false
+    }
   }
 }
 
