5f9fa8a6486c6ec408d070506f0df67f7e3efa4b
Owen <owenrees82@gmail.com>
2025-02-04 14:44:03 +0100

    logging updated for discord

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { PORT } from './utils/config'
 import logger from './utils/logger'
 import app from './app'
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   logger.info(`Server running on port ${PORT}`)
+  await logger.discord({ type: 'info', message: `Server started on port ${PORT}` })
 })
--- src/routes/pdf.ts
+++ src/routes/pdf.ts
@@ -1,38 +1,59 @@
 import { Router, Request, Response } from 'express'
 import strStream from 'string-to-stream'
 import latex from 'node-latex'
-
 import Pgn2Tex from '@owenrees/pgn2tex'
-
 import logger from '../utils/logger'
 
 const router = Router()
 
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   const { pgn, diagrams, diagramClock } = req.body
 
   try {
     const gameTex = new Pgn2Tex(pgn, diagrams, diagramClock).toTex()
     const texFile = strStream(gameTex)
-
     // @ts-ignore
     const pdf = latex(texFile)
+    let errorOccurred = false
 
-    pdf.on('error', (err) => {
-      logger.error(err)
-      throw new Error('PDF generation failed')
+    pdf.on('error', async (error) => {
+      errorOccurred = true
+      logger.error(error.message)
+      await logger.discord({ type: 'error', message: error.message })
+      if (!res.headersSent) {
+        res.setHeader('Content-Disposition', '')
+        res.setHeader('Content-Type', 'application/json')
+        res.status(500).json({ type: 'error', message: error.message })
+      }
     })
 
-    pdf.on('finish', () => {
-      logger.info('PDF Generated')
+    pdf.on('end', async () => {
+      if (!errorOccurred && !res.headersSent) {
+        logger.info('PDF Generated')
+        await logger.discord({ type: 'info', message: 'PDF Generated' })
+      }
     })
 
-    res.setHeader('Content-Disposition', 'inline')
-    res.setHeader('Content-Type', 'application/pdf')
-    res.status(201)
-    pdf.pipe(res)
+    if (!errorOccurred) {
+      res.setHeader('Content-Disposition', 'inline')
+      res.setHeader('Content-Type', 'application/pdf')
+      res.status(201)
+      pdf.pipe(res)
+    }
   } catch (error) {
-    res.status(500).json({ type: 'error', message: 'Something went wrong' })
+    if (error instanceof Error) {
+      logger.error(error.message)
+      await logger.discord({ type: 'error', message: error.message })
+      res.setHeader('Content-Disposition', '')
+      res.setHeader('Content-Type', 'application/json')
+      res.status(500).json({ type: 'error', message: error.message })
+    } else {
+      logger.error('Unknown Error')
+      await logger.discord({ type: 'error', message: 'Unknown Error' })
+      res.setHeader('Content-Disposition', '')
+      res.setHeader('Content-Type', 'application/json')
+      res.status(500).json({ type: 'error', message: 'Unknown Error' })
+    }
   }
 })
 
