aec75060b09fbaf0b9dc8e4ed25486cfc74563f7
Owen <owenrees.dev@gmail.com>
2026-05-26 22:14:26 +0200

    remove preamble strip code

--- src/utils/preamble.ts
+++
@@ -1,8 +0,0 @@
-export const stripPreambleFromTex = (texString: string) => {
-  return texString
-    .replace(/\\documentclass(?:\[.*?\])?\{.*?\}/g, '')
-    .replace(/\\usepackage(?:\[.*?\])?\{.*?\}/g, '')
-    .replace(/\\geometry\{.*?\}/g, '')
-    .replace(/\\setlength\{.*?\}.*?\{.*?\}/g, '') // setlength has two sets of braces
-    .trim()
-}
